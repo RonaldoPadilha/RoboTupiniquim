54de81bf79d4c575224e6267384d1d4aedffb60d
Ronaldo Padilha <ronaldocp762@gmail.com>
2026-05-07 14:59:56 -0300

    Adicionado processamento de comandos

--- RoboTupiniquim.ConsoleApp/Program.cs
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*  Teste do algoritmo
+    Posição Inicial: +
+    X = 1
+    Y = 2
+    D = N     Direções: N(Norte)- S(Sul)- L(Leste)- O(Oeste)
+    Comando = EMEMEMEMM 
+*/
+int posicaoX = 1;
+int posicaoY = 2;
+char orientação = 'N';
+
+string comandoCompleto = "EMEMEMEMM";
+
+Console.WriteLine($"Posição inicial: X={posicaoX} Y={posicaoY} O={orientação}");
+
+char[] instrucoes = comandoCompleto.ToCharArray();
+
+for (int contador = 0; contador < instrucoes.Length; contador++)
+{
+    char instrucao = instrucoes[contador];
+}
