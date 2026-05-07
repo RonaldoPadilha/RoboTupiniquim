5545678ac01e92b0aeaae5b66ce88c31a59319b8
Ronaldo Padilha <ronaldocp762@gmail.com>
2026-05-07 15:59:38 -0300

    Adicionado lógica de movimentação

--- RoboTupiniquim.ConsoleApp/Program.cs
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -7,15 +7,39 @@
 */
 int posicaoX = 1;
 int posicaoY = 2;
-char orientação = 'N';
+char orientacao = 'N';
 
 string comandoCompleto = "EMEMEMEMM";
 
-Console.WriteLine($"Posição inicial: X={posicaoX} Y={posicaoY} O={orientação}");
+Console.WriteLine($"Posição inicial: {posicaoX} {posicaoY} {orientacao}");
 
 char[] instrucoes = comandoCompleto.ToCharArray();
 
 for (int contador = 0; contador < instrucoes.Length; contador++)
 {
     char instrucao = instrucoes[contador];
+
+    if (instrucao == 'E')//Esquerda
+    {
+        if (orientacao == 'N') orientacao = 'O';
+        else if (orientacao == 'O') orientacao = 'S';
+        else if (orientacao == 'S') orientacao = 'L';
+        else if (orientacao == 'L') orientacao = 'N';
+    }
+    else if (instrucao == 'D')
+    {
+        if (orientacao == 'N') orientacao = 'L';
+        else if (orientacao == 'L') orientacao = 'S';
+        else if (orientacao == 'S') orientacao = 'O';
+        else if (orientacao == 'O') orientacao = 'N';
+    }
+    else if (instrucao == 'M')
+    {
+        if (orientacao == 'N') posicaoY++;
+        else if (orientacao == 'S') posicaoY--;
+        else if (orientacao == 'O') posicaoX--;
+        else if (orientacao == 'L') posicaoX++;
+    }
 }
+Console.WriteLine($"Posição final: {posicaoX} {posicaoY} {orientacao}");
+Console.ReadLine();
