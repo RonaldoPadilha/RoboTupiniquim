c37211732e06571928bd083deba7a23f3309d3e6
Ronaldo Padilha <ronaldocp762@gmail.com>
2026-05-07 17:31:08 -0300

    Adicionado classe estática e métodos de utilidade da classe Robô

--- RoboTupiniquim.ConsoleApp/Program.cs
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -1,45 +1,13 @@
-﻿/*  Teste do algoritmo
-    Posição Inicial: +
-    X = 1
-    Y = 2
-    D = N     Direções: N(Norte)- S(Sul)- L(Leste)- O(Oeste)
-    Comando = EMEMEMEMM 
-*/
-int posicaoX = 1;
-int posicaoY = 2;
-char orientacao = 'N';
-
-string comandoCompleto = "EMEMEMEMM";
-
-Console.WriteLine($"Posição inicial: {posicaoX} {posicaoY} {orientacao}");
-
-char[] instrucoes = comandoCompleto.ToCharArray();
-
-for (int contador = 0; contador < instrucoes.Length; contador++)
+﻿class Program
 {
-    char instrucao = instrucoes[contador];
+    static void Main()
+    {
+        Robo.Configurar(1, 2, 'N');
 
-    if (instrucao == 'E')//Esquerda
-    {
-        if (orientacao == 'N') orientacao = 'O';
-        else if (orientacao == 'O') orientacao = 'S';
-        else if (orientacao == 'S') orientacao = 'L';
-        else if (orientacao == 'L') orientacao = 'N';
-    }
-    else if (instrucao == 'D')
-    {
-        if (orientacao == 'N') orientacao = 'L';
-        else if (orientacao == 'L') orientacao = 'S';
-        else if (orientacao == 'S') orientacao = 'O';
-        else if (orientacao == 'O') orientacao = 'N';
-    }
-    else if (instrucao == 'M')
-    {
-        if (orientacao == 'N') posicaoY++;
-        else if (orientacao == 'S') posicaoY--;
-        else if (orientacao == 'O') posicaoX--;
-        else if (orientacao == 'L') posicaoX++;
+        Console.WriteLine($"Posição inicial: {Robo.posicaoX} {Robo.posicaoY} {Robo.orientacao}");
+
+        Robo.Executar("EMEMEMEMM");
+
+        Console.WriteLine($"Posição final: {Robo.posicaoX} {Robo.posicaoY} {Robo.orientacao}");
     }
 }
-Console.WriteLine($"Posição final: {posicaoX} {posicaoY} {orientacao}");
-Console.ReadLine();
